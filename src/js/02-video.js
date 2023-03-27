51bf6fb49ec5e852c7bd551d7722f66b6d145737
Stas Voievodin <vstasv@i.ua>
2023-03-27 16:01:08 +0300

    t2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+// localStorage.setItem("videoplayer-current-time", timeupdate);
+
+player.on('play', function (timeupdate) {
+    console.log(timeupdate)
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+console.log(player)
