7c760f75f2e58050b7789027bb931f0aea82cf1e
Stas Voievodin <vstasv@i.ua>
2023-03-30 15:23:49 +0300

    finish task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,43 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-// localStorage.setItem("videoplayer-current-time", timeupdate);
 
-player.on('play', function (timeupdate) {
-    console.log(timeupdate)
-    console.log('played the video!');
+player.on('timeupdate', throttle(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+    console.log(seconds);
+}, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSON.parse(savedTime);
+// console.log(parsedTime.seconds);
+
+player.setCurrentTime(parsedTime.seconds).then(function (seconds) {
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
 });
 
-player.getVideoTitle().then(function (title) {
-    console.log('title:', title);
-});
-console.log(player)
+
+
+//*Теж працює________________________
+// window.addEventListener('beforeunload', function () {
+//     player.getCurrentTime().then(function (seconds) {
+//         localStorage.setItem('videoplayer-current-time', seconds);
+//     });
+// });
+
+// player.ready().then(function () {
+//     const savedTime = localStorage.getItem('videoplayer-current-time');
+//     if (savedTime !== null) {
+//         player.setCurrentTime(savedTime);
+//     }
+// });
+
