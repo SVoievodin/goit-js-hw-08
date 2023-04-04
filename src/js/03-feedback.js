a854c4bad10331eafd6e886cf838088b5cfdde91
Stas Voievodin <vstasv@i.ua>
2023-04-04 14:17:33 +0300

    final-fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,7 +14,7 @@ if (parsedInputs !== null) {
 form.addEventListener('input', throttle(inputMonitor, 500));
 form.addEventListener('submit', submit);
 
-function inputMonitor(event) {
+function inputMonitor() {
     const inputFromForm = {
         email: inputEmail.value,
         message: inputMessage.value,
@@ -24,55 +24,14 @@ function inputMonitor(event) {
 }
 function submit(event) {
     event.preventDefault();
-    console.log(parsedInputs);
-    inputEmail.value = '';
-    inputMessage.value = '';
+    const dataLog = {
+        email: inputEmail.value,
+        message: inputMessage.value,
+    };
+    console.log(dataLog);
+    form.reset();
+    localStorage.removeItem('feedback-form-state')
 }
 
 
 
-
-
-
-
-
-
-// const form = document.querySelector('.feedback-form');
-// // console.dir(form.elements[0].value);
-
-// const savedInputs = localStorage.getItem('feedback-form-state');
-// const parsedInputs = JSON.parse(savedInputs);
-// // console.log(parsedInputs)
-// if (parsedInputs !== null) {
-//     form.elements[0].value = parsedInputs.email;
-//     form.elements[1].value = parsedInputs.message;
-// }
-// // form.addEventListener('submit', inputMonitor);
-
-// // form.addEventListener('input', inputMonitor);
-// form.addEventListener('sabmit', submit);
-// form.addEventListener('input', throttle(inputMonitor, 500));
-
-
-// function inputMonitor(event) {
-//     // console.dir(event.target)
-//     event.preventDefault();
-//     const inputFromForm = {
-//         email: event.currentTarget.elements[0].value,
-//         message: event.currentTarget.elements[1].value,
-//     }
-//     localStorage.setItem('feedback-form-state', JSON.stringify(inputFromForm))
-//     console.log(inputFromForm);
-// }
-// function submit(event) {
-//     // console.dir(event.target)
-//     event.preventDefault();
-//     // const inputFromForm = {
-//     //     email: event.currentTarget.elements[0].value,
-//     //     message: event.currentTarget.elements[1].value,
-//     // }
-//     // localStorage.setItem('feedback-form-state', JSON.stringify(inputFromForm))
-//     console.log(parsedInputs);
-//     event.currentTarget.reset();
-// }
-// form.reset();
